ad4cb2fda063034a0bba5b3e0f588cf32777eb10
Andi Cucka <AndiCucka>
2024-04-24 09:02:54 -0400

    code

--- Main.java
+++ Main.java
@@ -1,1 +1,31 @@
+/**
+ * The main program.
+ * 
+ * @author Andi Cucka
+ * @version 1.0
+ * @since 2024-04-13
+ */
+public class Main {
+
+    public static String reverseString(String stringToReverse) {
+        if (stringToReverse.isEmpty()) {
+            return "";
+        } else {
+            char firstCharacterOfString = stringToReverse.charAt(0);
+            String restOfString = stringToReverse.substring(1);
+            return reverseString(restOfString) + firstCharacterOfString;
+        }
+    }
+
+    public static void main(String[] args) {
+        String aString = "recursion";
+
+        System.out.println("");
+        System.out.println("The original string is: " + aString);
+        String theReversedString = reverseString(aString);
+        System.out.println("The reversed string is: " + theReversedString);
+
+        System.out.println("\nDone.");
+    }
+}
 
--- index.ts
+++ index.ts
@@ -1,1 +1,30 @@
+/*
+* The main program.
+*
+* @author  Andi Cucka
+* @version 1.0
+* @since   2024-04-13
+*/
 
+function reverseString(stringToReverse: string): string {
+    if (stringToReverse === "") {
+        return "";
+    } else {
+        const firstCharacterOfString = stringToReverse[0];
+        const restOfString = stringToReverse.slice(1);
+        return reverseString(restOfString) + firstCharacterOfString;
+    }
+}
+
+function main(): void {
+    const aString: string = "recursion";
+
+    console.log("");
+    console.log(`The original string is: ${aString}`);
+    const theReversedString: string = reverseString(aString);
+    console.log(`The reversed string is: ${theReversedString}`);
+
+    console.log("\nDone.");
+}
+
+main();
